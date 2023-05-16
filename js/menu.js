61d73f5dc9f65bc5486bf4d302ee9053e05791fd
daviolsilva <daviolsilva@hotmail.com>
2023-05-15 21:43:39 -0300

    update 15/05/2023 - rev.9

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,20 @@
-/*Arquivo das automações do Menu Hambúrguer */
+/*Arquivo das automações do Menu Hamburguer */
+
+const btnMobile = document.getElementById('btn-mobile');
+// Declarando a Variável com o CONST
+
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+        if (active) {
+            event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+        } else {
+            event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+        }
+} 
+
+btnMobile.addEventListener('click', toggleMenu)
+btnMobile.addEventListener('touchstart', toggleMenu)
